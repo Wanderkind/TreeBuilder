0a45ac358e7bb91930992b62e8ef15a50a71cf1d
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-06-12 04:33:54 +0900

    Update SubmissionNumberPredictor.py

--- SubmissionNumberPredictor.py
+++ SubmissionNumberPredictor.py
@@ -5,17 +5,17 @@ Y=[]
 input()
 z=input()
 input()
-for i in range(len(z)-30):
+for i in range(len(z)-22):
     if z[i:i+5]=='>4444':
         y=int(z[i+5:i+9])
         j=0
         while j<2*10**4:
             if z[i+j:i+j+10]=='2022-06-12':
-                h=(int(z[i+j+12])-3)*60
-                m=int(z[i+j+14:i+j+16])
+                h=(int(z[i+j+12])-4)*60
+                m=int(z[i+j+14:i+j+16])-40
                 s=int(z[i+j+17:i+j+19])
                 print(y,h+m,s)
-                X.append(60*(h+m)+s)
+                X.append((h+m)+s/60)
                 Y.append(y)
                 break
             else:
