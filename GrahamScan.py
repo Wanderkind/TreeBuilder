21909f43bef08edb4f0ab6a1d65d63bad49edf58
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-08-03 23:25:14 +0900

    Update GrahamScan.py

--- GrahamScan.py
+++ GrahamScan.py
@@ -4,12 +4,13 @@ from functools import reduce as R
 
 Q=lambda a:(a>0)-(a<0)
 T=lambda p,q,r:Q((q[0]-p[0])*(r[1]-p[1])-(r[0]-p[0])*(q[1]-p[1]))
-def _L(H,r):
-    while len(H)>1 and T(H[-2],H[-1], r)==-1:H.pop() # <1 to exclude colinear points
+def L(H,r):
+    while len(H)>1 and T(H[-2],H[-1],r)<1:H.pop() # ==-1 to include colinear points
     if not len(H) or H[-1]!=r:H.append(r)
     return H
 def c(P):
     P.sort()
-    l=R(_L,P,[])
-    u=R(_L,P[::-1],[])
+    l,u=R(L,P,[]),R(L,P[::-1],[])
     return l.extend(u[i] for i in range(1,len(u)-1)) or l
+
+# C=c([[*map(int,input().split())]for _ in range(int(input()))])
