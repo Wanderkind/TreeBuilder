92b85e01382adebab0ce2bf000c283f6a991ab66
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-08-03 22:03:20 +0900

    Create GrahamScan.py

---
+++ GrahamScan.py
@@ -0,0 +1,15 @@
+# credits to https://gist.github.com/arthur-e/5cf52962341310f438e96c1f3c3398b8
+
+from functools import reduce as R
+
+Q=lambda a:(a>0)-(a<0)
+T=lambda p,q,r:Q((q[0]-p[0])*(r[1]-p[1])-(r[0]-p[0])*(q[1]-p[1]))
+def _L(H,r):
+    while len(H)>1 and T(H[-2],H[-1], r)==-1:H.pop()
+    if not len(H) or H[-1]!=r:H.append(r)
+    return H
+def c(P):
+    P.sort()
+    l=R(_L,P,[])
+    u=R(_L,P[::-1],[])
+    return l.extend(u[i] for i in range(1,len(u)-1)) or l
