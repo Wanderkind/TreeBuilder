363b3aa80a1434204451c2b890f606b1dc8df092
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-10-25 04:59:43 +0900

    Create ipow.c

---
+++ ipow.c
@@ -0,0 +1,15 @@
+ll ipow(ll base, int exp){
+    
+	ll result = 1;
+    for (;;)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        if (!exp)
+            break;
+        base *= base;
+    }
+
+    return result;
+}
