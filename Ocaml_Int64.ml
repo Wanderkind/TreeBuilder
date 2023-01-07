f64e561c7be4c0cb33f3e25e80c8b9a5263c29c2
Jeongseop Lee <wanderkind@semyung.ac.kr>
2023-01-07 12:24:46 +0900

    Create Ocaml_Int64.ml

---
+++ Ocaml_Int64.ml
@@ -0,0 +1,17 @@
+open Int64
+;;
+let ( ** ) a b = mul a b
+let ( ++ ) a b = add a b
+let ( -- ) a b = sub a b
+let ( // ) a b = div a b
+let ( %% ) a b = rem a b
+;;
+
+(*
+let ( ** ) a b = Int64.mul a b
+let ( ++ ) a b = Int64.add a b
+let ( -- ) a b = Int64.sub a b
+let ( // ) a b = Int64.div a b
+let ( %% ) a b = Int64.rem a b
+;;
+*)
