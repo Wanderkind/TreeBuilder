280787c7de82cb6f15b224adf1eb177a016d19b1
Jeongseop Lee <wanderkind@semyung.ac.kr>
2023-10-20 16:52:13 +0900

    Update PrimeFactorization.py

--- PrimeFactorization.py
+++ PrimeFactorization.py
@@ -1,9 +1,13 @@
 from collections import Counter as C
 def fc(n):
-    l=[]
-    i=2
-    while n>1:
-        if n%i:i+=1
-        else:l.append(i);n/=i
+    l = []
+    i = 2
+    while n > 1:
+        if n % i:
+            i += 1 if i == 2 else 2
+        else:
+            l.append(i)
+            n //= i
     c=C(l)
-    return [(i,c[i])for i in c]
+    #return [(i, c[i]) for i in c]
+    return c
