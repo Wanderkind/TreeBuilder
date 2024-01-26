de15e4ec794653dbc539c1a7f1742a40f2a6577f
Jeongseop Lee <wanderkind@semyung.ac.kr>
2024-01-26 14:04:11 +0900

    Create Prefix_ActivePattern.fs

---
+++ Prefix_ActivePattern.fs
@@ -0,0 +1,12 @@
+let (|Prefix|_|) (p: string) (s: string) : string option =
+    if s.StartsWith(p) then
+        Some(s.Substring(p.Length))
+    else
+        None
+
+(*
+match "Hello world" with
+| Prefix "The" rest -> printfn "Started with 'The', rest is %s" rest
+| Prefix "Hello" rest -> printfn "Started with 'Hello', rest is %s" rest
+| _ -> printfn "neither"
+*)
