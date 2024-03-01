d0ab30e30cde7767eaa765695ba26a190ec30007
Jeongseop Lee <wanderkind@semyung.ac.kr>
2024-03-01 16:06:55 +0900

    Update decap.ml

--- decap.ml
+++ decap.ml
@@ -5,3 +5,9 @@ let decap f lst = f (List.hd lst) (List.tl lst)
 exapmle:
 https://www.acmicpc.net/source/share/087f3a5c00594d13858777aadac80b4d
 *)
+
+(*
+for F#:
+let decap f lst = f (List.head lst) (List.tail lst)
+;;
+*)
